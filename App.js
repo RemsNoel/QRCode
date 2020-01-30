76cb69e808786529f9b28e0b15a802a0d8f98890
RemsNoel <remy.noel@epsi.fr>
2020-01-30 13:51:15 +0100

    Creation Acceuil + mise en place de la navigation + debut page Inscription

--- App.js
+++ App.js
@@ -1,13 +1,83 @@
 // App.js
 
 import React from 'react'
+import 'react-native-gesture-handler'
+import Connexion from './Components/Connexion'
 import Search from './Components/Search'
-import Acceuil from './Components/Accueil'
+import { Button, View, Text, StyleSheet } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+class AccueilScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <View style={styles.second_container}>
+          <Connexion/>
+        </View>
+        <View style={styles.second_container}>
+          <View style={styles.buttons}>
+            <Button
+              title="Connexion"
+              onPress={() => this.props.navigation.navigate('Inscription')}
+            />
+          </View>
+          <View style={styles.buttons}>
+            <Button
+              title="Inscription"
+              onPress={() => this.props.navigation.navigate('Inscription')}
+            />
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+class InscriptionScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.main_container}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('Accueil')}
+        />
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Accueil: AccueilScreen,
+    Inscription: InscriptionScreen,
+  },
+  {
+    initialRouteName: 'Accueil',
+  }
+);
+
+
+const AppContainer = createAppContainer(RootStack);
+const styles = StyleSheet.create({
+  main_container: { 
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center' 
+  },
+  second_container: {     
+    flex: 1,
+    marginTop:5,
+    marginBottom: 5
+  },
+  buttons: {
+    marginTop: 5
+  }
+})
 
 export default class App extends React.Component {
   render() {
-    return (
-      <Acceuil/>
-    )
+    return <AppContainer />;
   }
 }
---
+++ Components/Connexion.js
@@ -0,0 +1,63 @@
+// Components/Connexion.js
+
+import React from 'react'
+import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+
+class Connexion extends React.Component {
+    render() {
+      return (
+        <View style={styles.main_container}>
+            <View style={styles.marge}></View>
+            <View style={styles.second_container}>          
+                <Text style={styles.text}>Identifiant :</Text>
+                <TextInput style={styles.textinput} placeholder='Identifiant'/>
+                <Text style={styles.text}>Mot de passe :</Text>
+                <TextInput style={styles.textinput} placeholder='Mot de passe'/>
+            </View>
+            <View style={styles.marge}></View>
+        </View>
+      )
+    }
+  }
+
+  const styles = StyleSheet.create({
+    main_container: {
+      flex: 1,
+      flexDirection:'row',
+      marginTop: 20
+    },
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop:5,
+        height: 50,
+        borderColor: '#000000',
+        borderWidth: 1,
+        paddingLeft: 5,
+        width: 200,
+    },
+    second_container :{     
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    buttons :{
+        flexDirection: 'column',
+        marginTop:5
+    },
+    buttonConn :{        
+        marginTop:5,
+    },
+    buttonRegister :{        
+        marginTop:5,
+        backgroundColor: 'red'
+    },
+    text :{
+        fontSize:20,
+        color : 'black'
+    },
+    marge :{
+        flex:1
+    }
+  })
+
+  export default Connexion
